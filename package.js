898a359000b033feed09eedb6f676c951716934f
wiserweb <wiserweb@users.noreply.github.com>
2016-03-31 03:17:05 -0400

    Update package.js
    updated cordova.depends to updated repo, updated version and package name

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-  name: 'hybrid:background',
+  name: 'wiserweb:background',
   summary: 'Allows Cordova apps to continue running in the background',
-  version: '1.0.0',
-  git: 'https://github.com/meteorhybrid/background'
+  version: '1.0.1',
+  git: 'https://github.com/wiserweb/background'
 });
 
 Cordova.depends({
-  'de.appplant.cordova.plugin.background-mode':'0.6.4'
+  'cordova-plugin-background-mode':'https://github.com/cvuser0/cordova-plugin-background-mode.git#67e6f1ed8a280b2035e99aefbe03d3eb7ceacb8d'
 });
 
 Package.onUse(function(api) {
